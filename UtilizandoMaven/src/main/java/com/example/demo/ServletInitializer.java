31151f5260f13a410e9ec3ce563457016d0cd9ac
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-08-11 12:34:00 +0200

    Terminado

---
+++ UtilizandoMaven/src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	public String saludos(@RequestParam(required = false, defaultValue = "") String nombre) {
+        return "".equals(nombre) ? "Hola Mundo!" : "Hola " + nombre;
+    }
+
+}
